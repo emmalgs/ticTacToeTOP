b10336d90f6157e5501573568ac5a1be6f64a80b
Emma Gerigscott <elgerig@gmail.com>
2022-11-17 14:38:18 -0800

    Switches between p1 and p2

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
 const SQUARE = document.querySelectorAll('.board-unit');
+const BOARD = document.getElementById('game-container');
+
+BOARD.addEventListener('click', () => {
+    if (BOARD.classList.contains('x')) {
+        BOARD.classList.remove('x')
+        BOARD.classList.add('circle')
+    }
+    else if (BOARD.classList.contains('circle')) {
+        BOARD.classList.remove('circle')
+        BOARD.classList.add('x')
+    }
+})
 
 SQUARE.forEach(unit => {
     unit.addEventListener('click', () => {
-                unit.classList.add('circle')
-                })
+        if (BOARD.classList.contains('x')) {
+                unit.classList.add('x')
+                } else if (BOARD.classList.contains('circle')) {
+                    unit.classList.add('circle')
+                }
+            });
             })
+
